83a17e39f05934f5b13ebe5b4890190140165a16
Jason Kha <jasonkha63@yahoo.com>
2020-08-09 15:50:14 -0500

    Added UI and Bat movement

--- Player/Player.gd
+++ Player/Player.gd
@@ -13,15 +13,18 @@ enum {
 
 var state = MOVE
 var velocity = Vector2.ZERO
-var roll_vector = Vector2.LEFT
+var roll_vector = Vector2.DOWN
+var stats = PlayerStats
 
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
 onready var swordHitbox = $HitboxPivot/SwordHitbox
+onready var hurtbox = $Hurtbox
 
 # Called when the node is ready, initialization
 func _ready():
+	stats.connect("no_health", self, "queue_free")
 	animationTree.active = true
 	swordHitbox.knockback_vector = roll_vector
 	print("Player ready")
@@ -83,3 +86,8 @@ func roll_animation_finished():
 	
 func attack_animation_finished():
 	state = MOVE
+
+func _on_Hurtbox_area_entered(area):
+	stats.health -= 1
+	hurtbox.start_invincibility(0.5)
+	hurtbox.create_hit_effect()
